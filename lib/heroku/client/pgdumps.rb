1de4ec33056465f0b82d29198542a8d2cafd9777
Adam Wiggins <adam@heroku.com>
2010-04-19 14:24:16 -0700

    to_s on restclient response

--- lib/heroku/client/pgdumps.rb
+++ lib/heroku/client/pgdumps.rb
@@ -1,18 +1,18 @@
 class Heroku::Client
   def pgdumps(app_name)
-    JSON.parse(get("/apps/#{app_name}/pgdumps", :accept => 'application/json'))
+    JSON.parse(get("/apps/#{app_name}/pgdumps", :accept => :json).to_s)
   end
 
   def pgdump_capture(app_name)
-    JSON.parse(post("/apps/#{app_name}/pgdumps", :accept => 'application/json'))
+    JSON.parse(post("/apps/#{app_name}/pgdumps", :accept => :json).to_s)
   end
 
   def pgdump_restore(app_name, pgdump_name)
-    JSON.parse(put("/apps/#{app_name}/pgdumps", {:name => pgdump_name}, :accept => 'application/json'))
+    JSON.parse(put("/apps/#{app_name}/pgdumps", {:name => pgdump_name}, :accept => :json).to_s)
   end
 
   def pgdump_info(app_name, pgdump_name)
-    JSON.parse(get("/apps/#{app_name}/pgdumps/#{pgdump_name}", :accept => 'application/json'))
+    JSON.parse(get("/apps/#{app_name}/pgdumps/#{pgdump_name}", :accept => :json).to_s)
   end
 
   def pgdump_complete?(app_name, pgdump_name)
@@ -20,6 +20,6 @@ class Heroku::Client
   end
 
   def pgdump_url(app_name, pgdump_name)
-    JSON.parse(get("/apps/#{app_name}/pgdumps/#{pgdump_name || 'latest'}?temp_url=true", :accept => 'application/json'))['temp_url']
+    JSON.parse(get("/apps/#{app_name}/pgdumps/#{pgdump_name || 'latest'}?temp_url=true", :accept => :json).to_s)['temp_url']
   end
 end
